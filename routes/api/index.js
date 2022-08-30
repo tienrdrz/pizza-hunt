313cb0dc3927e66a2695f61b3b228d9e886392ca
etienne <etiennerodriguez3@gmail.com>
2022-08-30 12:16:41 -0400

    lesson 2 finished

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
 const pizzaRoutes = require('./pizza-routes');
 
-//add prefix of /pizzas to routes createed in pizzaroutes
-router.use('/pizzas', pizzaRoutes)
+router.use('/comments', commentRoutes);
+router.use('/pizzas', pizzaRoutes);
 
 module.exports = router;
